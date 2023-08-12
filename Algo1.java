e41387b0a6aaaa371910625d5ebf98c033b473ce
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-12 15:53:18 +0530

    Algorithm 1 completed

---
+++ Algo1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Algo1 {
+    private static Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
+
+        int maxNum;
+
+        do {
+            System.out.println();
+            System.out.print("Enter a positive number: ");
+            maxNum = scanner.nextInt();
+            scanner.nextLine();
+
+            if (maxNum <=0) System.out.println("Invalid. Enter only positive numbers");
+
+        } while (maxNum <= 0);
+
+        int num1 = 0;
+        int num2 = 1;
+        int tempNum;
+        System.out.println();
+        
+        do {
+
+            System.out.print(num1+", ");
+
+            tempNum = num2;
+            num2 += num1;
+            num1 = tempNum;
+            
+        } while (num1 < maxNum);
+
+        
+
+
+    }
+
+}
