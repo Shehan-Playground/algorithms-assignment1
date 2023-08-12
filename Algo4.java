29fae4a71ddcdcbff2cad15d5ac134d7440ae30e
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-12 22:16:31 +0530

    Algorithm 4 completed

---
+++ Algo4.java
@@ -0,0 +1,21 @@
+public class Algo4 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < nums.length/2; i++) {
+            int tempNum = nums[i];
+            nums[i] = nums[nums.length-1-i];
+            nums[nums.length-1-i] = tempNum;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+", ");
+        }
+
+        System.out.print("\b\b");
+        System.out.println("  ");
+        System.out.println();
+    }
+}
