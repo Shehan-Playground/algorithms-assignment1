cfe7424cad2f67df75f443e18525be3885995ea5
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-12 22:02:03 +0530

    Algorithm 3 completed

---
+++ Algo3.java
@@ -0,0 +1,24 @@
+public class Algo3 {
+    public static void main(String[] args) {
+        
+        int[] nums = {10, 20, 30, 40, 50};
+
+        int[] newArray = new int[5];
+
+        for (int i = 0; i < nums.length; i++) {
+            newArray[i] = nums[nums.length-1-i];
+        }
+
+        nums = newArray;
+
+        System.out.println();
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+", ");
+        }
+        System.out.print("\b\b");
+        System.out.println("  ");
+        System.out.println();
+
+    }
+}
