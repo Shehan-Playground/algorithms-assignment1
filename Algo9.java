ccc8bdf935e54191bc6f7bd8d2fdc7a23af36756
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-13 12:19:19 +0530

    Algorithm 9 completed

---
+++ Algo9.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Algo9 {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        String inputText;
+        boolean valid;
+
+        do {
+            valid = true;
+
+            System.out.println();
+            System.out.print("Enter a single word: ");
+            inputText = scanner.nextLine().strip();
+
+            if (inputText.isBlank()) {
+                System.out.println("Input cannot be blank or only spaces");
+                continue;
+            }
+
+            if (inputText.contains(" ")) {
+                System.out.println("Input only a single word");
+                valid = false;
+            }
+
+        } while (inputText.isBlank() || !valid);
+
+        char[] inputTextArray = inputText.toLowerCase().toCharArray();
+
+        //Check palindrome
+        int i;
+        for (i = 0; i < inputTextArray.length/2; i++) {
+            if (inputTextArray[i] != inputTextArray[inputTextArray.length-1-i]) {
+                System.out.println("Not palindrome");
+                break;
+            }
+        }
+        if (i == inputTextArray.length/2) System.out.println("It's a palindrom");
+
+    }
+}
